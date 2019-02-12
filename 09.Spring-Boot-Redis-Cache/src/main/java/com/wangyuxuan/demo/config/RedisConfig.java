28f824060b9fe58af310d12e3fd137a417212184
wangyuxuan <165140760@qq.com>
2019-02-12 17:24:57 +0800

    Spring Boot中使用缓存

---
+++ 09.Spring-Boot-Redis-Cache/src/main/java/com/wangyuxuan/demo/config/RedisConfig.java
@@ -0,0 +1,92 @@
+package com.wangyuxuan.demo.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @Author: wangyuxuan
+ * @Date: 2019/2/12 15:25
+ * @Description: Redis 配置类
+ */
+
+@Slf4j
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+
+    @Autowired
+    private JedisConnectionFactory jedisConnectionFactory;
+
+    /**
+     * 自定义缓存key生成策略
+     */
+    @Override
+    @Bean
+    public KeyGenerator keyGenerator() {
+        // 设置自动key的生成规则，配置spring boot的注解，进行方法级别的缓存
+        // 使用：进行分割，可以显示出很多层级关系
+        // 这里其实就是new了一个KeyGenerator对象，只是这是lambda表达式的写法，我感觉很好用，大家感兴趣可以去了解下
+        return (target, method, params) -> {
+            StringBuilder sb = new StringBuilder();
+            sb.append(target.getClass().getName());
+            sb.append(":");
+            sb.append(method.getName());
+            for (Object obj : params) {
+                sb.append(":" + obj.toString());
+            }
+            log.info("自动生成Redis Key -> [{}]", sb.toString());
+            return sb.toString();
+        };
+    }
+
+    /**
+     * 缓存管理器
+     */
+    @Override
+    @Bean
+    public CacheManager cacheManager() {
+        // 初始化缓存管理器，在这里我们可以缓存的整体过期时间什么的，我这里默认没有配置
+        log.info("初始化 -> [{}]", "CacheManager RedisCacheManager Start");
+        RedisCacheManager.RedisCacheManagerBuilder builder = RedisCacheManager
+                .RedisCacheManagerBuilder
+                .fromConnectionFactory(jedisConnectionFactory);
+        return builder.build();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(JedisConnectionFactory jedisConnectionFactory) {
+        // 设置序列化
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        // 配置redisTemplate
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory);
+        RedisSerializer stringSerializer = new StringRedisSerializer();
+        // key序列化
+        redisTemplate.setKeySerializer(stringSerializer);
+        // value序列化
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        // Hash key序列化
+        redisTemplate.setHashKeySerializer(stringSerializer);
+        // Hash value序列化
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+}
---
+++ 09.Spring-Boot-Redis-Cache/src/test/java/com/wangyuxuan/demo/DemoApplicationTests.java
@@ -0,0 +1,40 @@
+package com.wangyuxuan.demo;
+
+import com.wangyuxuan.demo.bean.Student;
+import com.wangyuxuan.demo.service.StudentService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+    @Autowired
+    private StudentService studentService;
+
+    @Test
+    public void test1() throws Exception{
+        Student student1 = this.studentService.queryStudentBySno("001");
+        System.out.println("学号" + student1.getSno() + "的学生姓名为：" + student1.getName());
+
+        Student student2 = this.studentService.queryStudentBySno("001");
+        System.out.println("学号" + student2.getSno() + "的学生姓名为：" + student2.getName());
+    }
+
+    @Test
+    public void test2() throws Exception{
+        Student student1 = this.studentService.queryStudentBySno("001");
+        System.out.println("学号" + student1.getSno() + "的学生姓名为：" + student1.getName());
+
+        student1.setName("康康");
+        this.studentService.update(student1);
+
+        Student student2 = this.studentService.queryStudentBySno("001");
+        System.out.println("学号" + student2.getSno() + "的学生姓名为：" + student2.getName());
+    }
+
+}
+
