271076764b6862c2fde32c3565b1b4e3fc9e53b1
wangyuxuan <165140760@qq.com>
2019-02-15 16:26:28 +0800

    使用Spring Boot发送邮件

---
+++ 15.Spring-Boot-Email/src/main/java/com/wangyuxuan/demo/controller/EmailController.java
@@ -0,0 +1,130 @@
+package com.wangyuxuan.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import javax.mail.internet.MimeMessage;
+
+/**
+ * @Author: wangyuxuan
+ * @Date: 2019/2/15 11:08
+ * @Description:
+ */
+
+@RestController
+@RequestMapping("/email")
+public class EmailController {
+
+    @Autowired
+    private JavaMailSender jms;
+
+    @Value("${spring.mail.username}")
+    private String from;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @RequestMapping("sendSimpleEmail")
+    public String sendSimpleEmail() {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(from);
+            message.setTo("472848022@qq.com");
+            message.setSubject("王宇轩Spring Boot测试");
+            message.setText("使用Spring Boot发送简单邮件。");
+            jms.send(message);
+            return "发送成功";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+    }
+
+    @RequestMapping("sendHtmlEmail")
+    public String sendHtmlEmail() {
+        MimeMessage message = null;
+        try {
+            message = jms.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(from);
+            helper.setTo("472848022@qq.com");
+            helper.setSubject("王宇轩Spring Boot测试");
+            StringBuffer sb = new StringBuffer("<p style='color:#42b983'>使用Spring Boot发送HTML格式邮件。</p>");
+            helper.setText(sb.toString(), true);
+            jms.send(message);
+            return "发送成功";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+    }
+
+    @RequestMapping("sendAttachmentsMail")
+    public String sendAttachmentsMail() {
+        MimeMessage message = null;
+        try {
+            message = jms.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(from);
+            helper.setTo("472848022@qq.com");
+            helper.setSubject("王宇轩Spring Boot测试");
+            helper.setText("详情参见附件内容！");
+            ClassPathResource file = new ClassPathResource("/static/file/项目文档.docx", this.getClass().getClassLoader());
+            helper.addAttachment("项目文档.docx", file);
+            jms.send(message);
+            return "发送成功";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+    }
+
+    @RequestMapping("sendInlineMail")
+    public String sendInlineMail() {
+        MimeMessage message = null;
+        try {
+            message = jms.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(from);
+            helper.setTo("472848022@qq.com");
+            helper.setSubject("王宇轩Spring Boot测试");
+            helper.setText("<html><body>博客图：<img src='cid:img'/></body></html>", true);
+            ClassPathResource file = new ClassPathResource("/static/img/sunshine.png", this.getClass().getClassLoader());
+            helper.addInline("img", file);
+            jms.send(message);
+            return "发送成功";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+    }
+
+    @RequestMapping("sendTemplateEmail")
+    public String sendTemplateEmail(String code) {
+        MimeMessage message = null;
+        try {
+            message = jms.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(from);
+            helper.setTo("472848022@qq.com");
+            helper.setSubject("王宇轩Spring Boot测试");
+            Context context = new Context();
+            context.setVariable("code", code);
+            String template = templateEngine.process("emailTemplate", context);
+            helper.setText(template, true);
+            jms.send(message);
+            return "发送成功";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+    }
+}
